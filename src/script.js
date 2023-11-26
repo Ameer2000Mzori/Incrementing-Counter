e0343254d7310624351a85da87d055573fd41289
Ameer <ameerapex496@gmail.com>
2023-11-26 17:31:25 +0100

    added function to the project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,51 @@
-console.log("hallo world");
+// selecting our elements
+var twitterCount = document.getElementsByClassName("twitter-Count")[0];
+var youtubeCount = document.getElementsByClassName("youtube-Count")[0];
+var facebookCount = document.getElementsByClassName("facebook-Count")[0];
+// our functions
+// twitter increment function
+// creating global varibale
+var twCounter = 0;
+var twitterFunc = function () {
+    // our interval
+    var twitterInv = setInterval(function () {
+        twCounter += 100;
+        twitterCount.textContent = "".concat(twCounter);
+        // if value equal then stop interval
+        if (twCounter === 12000) {
+            clearInterval(twitterInv);
+        }
+    }, 25);
+};
+// youtube increment function
+// creating global varibale
+var ytCounter = 0;
+var youtubeFunc = function () {
+    // our interval
+    var youtubeInv = setInterval(function () {
+        ytCounter += 100;
+        youtubeCount.textContent = "".concat(ytCounter);
+        // if value equal then stop interval
+        if (ytCounter === 5000) {
+            clearInterval(youtubeInv);
+        }
+    }, 50);
+};
+// facebook increment function
+// creating global varibale
+var fbCounter = 0;
+var facebookFunc = function () {
+    // our interval
+    var facebookInv = setInterval(function () {
+        fbCounter += 125;
+        facebookCount.textContent = "".concat(fbCounter);
+        // if value equal then stop interval
+        if (fbCounter === 7500) {
+            clearInterval(facebookInv);
+        }
+    }, 25);
+};
+// our eventlisnters on load fire this function
+window.addEventListener("load", twitterFunc);
+window.addEventListener("load", youtubeFunc);
+window.addEventListener("load", facebookFunc);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,63 @@
-console.log("hallo world");
+// selecting our elements
+const twitterCount = document.getElementsByClassName("twitter-Count")[0] as any;
+const youtubeCount = document.getElementsByClassName("youtube-Count")[0] as any;
+const facebookCount = document.getElementsByClassName(
+  "facebook-Count"
+)[0] as any;
+
+// our functions
+
+// twitter increment function
+
+// creating global varibale
+let twCounter: number = 0;
+
+const twitterFunc = () => {
+  // our interval
+  const twitterInv = setInterval(() => {
+    twCounter += 100;
+    twitterCount.textContent = `${twCounter}`;
+    // if value equal then stop interval
+    if (twCounter === 12000) {
+      clearInterval(twitterInv);
+    }
+  }, 25);
+};
+
+// youtube increment function
+
+// creating global varibale
+let ytCounter: number = 0;
+
+const youtubeFunc = () => {
+  // our interval
+  const youtubeInv = setInterval(() => {
+    ytCounter += 100;
+    youtubeCount.textContent = `${ytCounter}`;
+    // if value equal then stop interval
+    if (ytCounter === 5000) {
+      clearInterval(youtubeInv);
+    }
+  }, 50);
+};
+// facebook increment function
+
+// creating global varibale
+let fbCounter: number = 0;
+
+const facebookFunc = () => {
+  // our interval
+  const facebookInv = setInterval(() => {
+    fbCounter += 125;
+    facebookCount.textContent = `${fbCounter}`;
+    // if value equal then stop interval
+    if (fbCounter === 7500) {
+      clearInterval(facebookInv);
+    }
+  }, 25);
+};
+
+// our eventlisnters on load fire this function
+window.addEventListener("load", twitterFunc);
+window.addEventListener("load", youtubeFunc);
+window.addEventListener("load", facebookFunc);
